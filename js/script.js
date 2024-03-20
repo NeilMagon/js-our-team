c5c5af7bdc6fb609978dc3c7d5987f9640b61ca9
NeilMagon <neil.magon97@gmail.com>
2024-03-20 18:21:25 +0100

    feat: add header

--- js/script.js
+++ js/script.js
@@ -61,16 +61,23 @@ const teamMembers = [
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 
 // Seleziono la mia ul tramite id
-const ourMembers = document.querySelector(`#our-members`);
+// const ourMembers = document.querySelector(`#our-members`);
 
-for (let i = 0; i < teamMembers.length; i++) {
-    const thisTeam = teamMembers[i];
-    const domLI = members(thisTeam.name, thisTeam.role, thisTeam.image);
-}
+// for (let i = 0; i < teamMembers.length; i++) {
+//     const thisTeam = teamMembers[i];
+//     const domLI = members(thisTeam.name, thisTeam.role, thisTeam.image);
+// }
 
 // Creo una funzione per stampare su dom le informazioni
-function members(teamName, teamRole, teamImage) {
-    const newLi = document.createElement('li');
-    newLi.textContent = `Nome: ${teamName}, Ruolo: ${teamRole}, Immagine: ${teamImage}`;
-    ourMembers.appendChild(newLi);
-}
+// function members(teamName, teamRole, teamImage) {
+//     const newLi = document.createElement('li');
+//     newLi.textContent = `Nome: ${teamName}, Ruolo: ${teamRole}, Immagine: ${teamImage}`;
+//     ourMembers.appendChild(newLi);
+// }
+
+
+
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
