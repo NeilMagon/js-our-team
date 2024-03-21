55c499b1896b96912a02bb996e8bac6ad4227f22
NeilMagon <neil.magon97@gmail.com>
2024-03-21 10:24:22 +0100

    feat: fix js

--- js/script.js
+++ js/script.js
@@ -78,61 +78,24 @@ const teamMembers = [
 
 // BONUS 1:
 // Trasformare la stringa foto in una immagine effettiva
-function createImgElement(src){
-    const imgElem = document.createElement(`img`);
-    imgElem.setAttribute(`src`, src);
-    return imgElem;
-}
 // BONUS 2:
 // Organizzare i singoli membri in card/schede
 const cardContainer = document.querySelector('#card-container');
 
-// Ciclo e creazione delle schede per ciascuno di essi
 for (let i = 0; i < teamMembers.length; i++) {
     const thisTeam = teamMembers[i];
-    const card = createCard(thisTeam.name, thisTeam.role, thisTeam.image);
-    cardContainer.appendChild(card); // Aggiungo la scheda al contenitore delle schede
+    const newElement = `
+        <div class="card col-4 my-3">
+            <div clsdd="card-body">
+                <div class="card-img">
+                    <img src="${thisTeam.image}" alt="">
+                </div>
+                <div class="card-text">
+                    <p>Nome: ${thisTeam.name}</p>
+                    <p>Ruolo: ${thisTeam.role}</p>
+                </div>
+            </div>
+        </div>`;
+    cardContainer.innerHTML += newElement
 }
-
-// Funzione per creare una scheda per un membro del team
-function createCard(nome, ruolo, foto) {
-    // Creo l'elemento di scheda
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.classList.add('col-4');
-    card.classList.add('my-3');
-
-    // Creo l'elemento immagine
-    const imgElem = createImgElement(foto);
-    imgElem.classList.add('card-img');
-
-    // Creo il corpo della scheda
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
-
-    // Creo il paragrafo per il nome
-    const nameP = document.createElement('p');
-    nameP.classList.add('card-title');
-    nameP.textContent = `Nome: ${nome}`;
-
-    // Creo il paragrafo per il ruolo
-    const roleP = document.createElement('p');
-    roleP.classList.add('card-text');
-    roleP.textContent = `Ruolo: ${ruolo}`;
-
-    // Aggiungo gli elementi al corpo della scheda
-    cardBody.appendChild(nameP);
-    cardBody.appendChild(roleP);
-
-    // Aggiungo l'immagine e il corpo della scheda alla scheda stessa
-    card.appendChild(imgElem);
-    card.appendChild(cardBody);
-
-    return card;
-}
-
-
-
-
-
 
